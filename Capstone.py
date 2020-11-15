1e396673859dc5008bb54e35995ff050cc4bee81
umashankar0705 <74495438+umashankar0705@users.noreply.github.com>
2020-11-15 18:55:27 +0530

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Datascience Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
